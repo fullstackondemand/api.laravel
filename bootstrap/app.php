418263af96f3a25c7c95f928ad41962eb7bf451c
Pramod Singh Gusain <spramodgusain@gmail.com>
2025-08-29 16:32:14 +0530

    fix(json):  exception error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,17 +3,31 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+
+        // Not Found
+        $exceptions->render(function (NotFoundHttpException $e, $req) {
+            if ($req->acceptsJson())
+                return response()->json(['status' => false, 'error' => 'The requested resource was not found.'], 404);
+        });
+
+        // Method Not Allowed
+        $exceptions->render(function (MethodNotAllowedHttpException $e, $req) {
+            if ($req->acceptsJson())
+                return response()->json(['status' => false, 'error' => 'The requested method is not allowed for this endpoint.'], 405);
+        });
+
     })->create();
