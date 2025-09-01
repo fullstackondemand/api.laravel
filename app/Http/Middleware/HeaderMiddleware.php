b4f59da8b5e4791e41952aa75531e87f49a1d2c3
Pramod Singh Gusain <spramodgusain@gmail.com>
2025-09-01 13:06:08 +0530

    fix: header middleware

--- app/Http/Middleware/HeaderMiddleware.php
+++ app/Http/Middleware/HeaderMiddleware.php
@@ -1,14 +1,12 @@
 <?php
-
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class HeaderMiddleware
 {
-    public function handle(Request $req, Closure $next): JsonResponse
+    public function handle($req, Closure $next): Response
     {
         return $next($req)->withHeaders([
             'X-Version' => config('app.version')
